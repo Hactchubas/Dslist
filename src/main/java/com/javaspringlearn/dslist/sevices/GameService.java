b8ef26a697fce1df0eb67deef79866e235e86bdb
Kauã S. Maia <kauamaia@yahoo.com.br>
2023-07-13 01:01:27 -0300

    GET game by id

--- src/main/java/com/javaspringlearn/dslist/sevices/GameService.java
+++ src/main/java/com/javaspringlearn/dslist/sevices/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.javaspringlearn.dslist.dto.GameDTO;
 import com.javaspringlearn.dslist.dto.GameMinDTO;
 import com.javaspringlearn.dslist.entities.Game;
 import com.javaspringlearn.dslist.repositories.GameRepositorie;
@@ -18,5 +20,11 @@ public class GameService {
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();	
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return  new GameDTO(result);
 	}
 }
